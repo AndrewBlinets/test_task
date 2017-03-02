d4391c688930fb69195e443feb496e1e66844dfd
Andrew <andreiblinets@gmail.com>
2017-03-02 23:28:23 +0300

    Create Constans class for URL

---
+++ app/src/main/java/by/andrewblinets/test_task/Constans.java
@@ -0,0 +1,13 @@
+package by.andrewblinets.test_task;
+
+/**
+ * Created by Андрей on 02.03.2017.
+ */
+
+public class Constans {
+
+    public static final String HOST = "https://www.reddit.com";
+    public static final String Url_top = "/r/EarthPorn/top/.json?limit=100";
+    public static final String Url_new = "/r/EarthPorn/new/.json?limit=100";
+
+}
