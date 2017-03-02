5a9bd2c3a32c36607e787f7a005be63d56737de3
Andrew <andreiblinets@gmail.com>
2017-03-02 23:35:26 +0300

    Create class RestApi

---
+++ app/src/main/java/by/andrewblinets/test_task/RestApi.java
@@ -0,0 +1,34 @@
+package by.andrewblinets.test_task;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Андрей on 02.03.2017.
+ */
+
+public class RestApi {
+
+    private static Retrofit getRetrofitInstance()
+    {
+        return new Retrofit.Builder().baseUrl(Constans.HOST)
+                .addConverterFactory(GsonConverterFactory.create( )).build();
+    }
+
+    private static Retrofit getRetrofitInstanceImage(String URL)
+    {
+        return new Retrofit.Builder().baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create( )).build();
+    }
+
+    public static Api_tes_task getApi() {
+        return  getRetrofitInstance().create(Api_tes_task.class);
+    }
+
+
+    public static Api_tes_task getImageApi(String URL) {
+
+        return getRetrofitInstanceImage(URL).create(Api_tes_task.class);
+    }
+
+}
